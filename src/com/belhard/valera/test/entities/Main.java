2391965a71383431133f6b5ebdff9238fa76de09
unknown <valera.krivorot@gmail.com>
2019-08-27 21:14:53 +0300

    Less3

--- src/com/belhard/valera/test/entities/Main.java
+++ src/com/belhard/valera/test/entities/Main.java
@@ -3,6 +3,8 @@ package com.belhard.valera.test.entities;
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("Hello");
 
     }
---
+++ src/com/belhard/valera/test/entities/Type.java
@@ -0,0 +1,42 @@
+package com.belhard.valera.test.entities;
+
+public class Type {
+
+    public static void main(String[] args) {
+
+
+      /*  int a = 10;
+        int b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println(15%4);
+
+        byte r = (byte) 129;
+        System.out.println(r);
+        */
+
+
+
+
+       /* int a = 12;
+        int b = 12;
+        if (a>b) {
+
+            System.out.println("GOGOGO");
+        } else if (a<b) {
+
+            System.out.println("MIMIMI");
+
+        }
+        else {
+            System.out.println("Poshel nah");
+        }
+        System.out.println("End");
+    */
+
+
+
+
+    }
+}
