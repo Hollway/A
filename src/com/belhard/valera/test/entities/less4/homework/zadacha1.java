8815ed3226b7d50597a81a4d67fc91f3e2f7ddde
unknown <valera.krivorot@gmail.com>
2019-09-03 18:43:54 +0300

    1

--- src/com/belhard/valera/test/entities/less4/homework/zadacha1.java
+++ src/com/belhard/valera/test/entities/less4/homework/zadacha1.java
@@ -4,11 +4,13 @@ public class zadacha1 {
     public static void main(String[] args) {
 
 
-        String[][] arrayYmn = new String[9][];
-        for (int i = 0; i < arrayYmn.length; i++) {
-            for (int j = 0; j < arrayYmn.length; j++) {
-
-               System.out.printf("%3d", (i+1) * (j+1) );
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == 0 && j == 0) {
+                    System.out.print("   ");
+                    continue;
+                }
+                System.out.printf("%3d", (i + 1) * (j + 1));
             }
             System.out.println();
 
