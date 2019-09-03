9b3b940334eafd8ab16e8d052aa8b47bef04d7b9
unknown <valera.krivorot@gmail.com>
2019-09-03 19:45:03 +0300

    1

--- src/com/belhard/valera/test/entities/less4/homework/zadacha2/Book.java
+++ src/com/belhard/valera/test/entities/less4/homework/zadacha2/Book.java
@@ -1,12 +1,45 @@
 package com.belhard.valera.test.entities.less4.homework.zadacha2;
 
 
-
 public class Book {
+
+            // Поля
 
     private String title;
     private int pages;
     private ISBN isbn;
-    private Format dimensions;
+    private Format format;
 
+            // Конструктор
+
+    public Book(String title, int pages, ISBN isbn, Format format) {
+
+        if (validateParams(title, pages)) {                                 // Валидация
+            this.title = title;
+            this.pages = pages;
+        } else {
+            this.title = "Default";
+            this.pages = 10;
+        }
+        this.isbn = isbn;
+        this.format = format;
+    }
+
+    private boolean validateParams(String title, int pages) {             // Условие валидации
+        return title.length() > 1 & pages >= 10;
+    }
+
+    public void throughOutPage(){
+        this.pages--;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pages=" + pages +
+                ", isbn=" + isbn +
+                ", format=" + format +
+                '}';
+    }
 }
--- src/com/belhard/valera/test/entities/less4/homework/zadacha2/Main.java
+++ src/com/belhard/valera/test/entities/less4/homework/zadacha2/Main.java
@@ -1,4 +1,18 @@
 package com.belhard.valera.test.entities.less4.homework.zadacha2;
 
+import java.util.Date;
+
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Book1", 1,
+                    new ISBN("fn2", new Date()), Format.A4);
+
+        System.out.println(book1);
+        book1.throughOutPage();
+        book1.throughOutPage();
+        book1.throughOutPage();
+        book1.throughOutPage();
+
+        System.out.println(book1);
+    }
 }
